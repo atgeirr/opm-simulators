0831fdde631ed5bbef6e59baf4c6fc5d83376c64
hnil <Halvor.M.Nilsen@sintef.no>
2018-02-07 17:25:57 +0100

    changes to make adjoint solve

--- opm/autodiff/BlackoilWellModel_impl.hpp
+++ opm/autodiff/BlackoilWellModel_impl.hpp
@@ -542,13 +542,14 @@ namespace Opm {
     template<typename TypeTag>
     void
     BlackoilWellModel<TypeTag>::
-    rhsAdjointRes(const BVector& lam , BVector& rhs)
+    rhsAdjointRes(BVector& rhs)
     {
         if ( ! localWellsActive() ) {
             return;
         }
         for (auto& well : well_container_) {
-            well->rhsAdjointRes(lam, rhs);
+            well->rhsAdjointRes(rhs);
+            well->rhsAdjointWell();
         }
     }
 
@@ -574,7 +575,18 @@ namespace Opm {
         Ax.axpy( alpha, scaleAddRes_ );
     }
 
+    template<typename TypeTag>
+    void
+    BlackoilWellModel<TypeTag>::
+    recoverWellAdjointAndUpdateWellAdjoint(const BVector& x)
+    {
+        if (!localWellsActive())
+            return;
 
+        for (auto& well : well_container_) {
+            well->recoverWellAdjointAndUpdateAdjointState(x, well_state_);
+        }
+    }
 
 
 
