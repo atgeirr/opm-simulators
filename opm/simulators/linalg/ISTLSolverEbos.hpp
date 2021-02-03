01a1b5884652bf9b7c8fa09fb2974405a2f50a0f
Atgeirr Flø Rasmussen <atgeirr@sintef.no>
2021-02-03 14:49:17 +0100

    Avoid using unnecessary static variable.

--- opm/simulators/linalg/ISTLSolverEbos.hpp
+++ opm/simulators/linalg/ISTLSolverEbos.hpp
@@ -193,7 +193,7 @@ namespace Opm
 
         void prepare(const Matrix& M, Vector& b)
         {
-            static bool firstcall = true;
+            const bool firstcall = (matrix_ == nullptr);
 #if HAVE_MPI
             if (firstcall && parallelInformation_.type() == typeid(ParallelISTLInformation)) {
                 // Parallel case.
@@ -223,7 +223,6 @@ namespace Opm
                 makeOverlapRowsInvalid(getMatrix());
             }
             prepareFlexibleSolver();
-            firstcall = false;
         }
 
 
